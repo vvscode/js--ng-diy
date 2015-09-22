0ac132316a3d5019776c07a9f753894b278b4329
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-22 20:29:15 +0300

    triggers chained watchers in the same digest

--- test/scope.js
+++ test/scope.js
@@ -1,31 +1,42 @@
 /* jshint globalstrict: true */
 'use strict';
 
-function initWatchVal() {};
+function initWatchVal() {
+};
 
 
-function Scope(){
+function Scope() {
   this.$$watchers = [];
 };
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn || function() {},
+    listenerFn: listenerFn || function() {
+    },
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function() {
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function() {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
-    if(newValue !== oldValue) {
+    if (newValue !== oldValue) {
       watcher.last = newValue;
-      watcher.listenerFn(newValue, (oldValue === initWatchVal)? newValue: oldValue, self);
+      watcher.listenerFn(newValue, (oldValue === initWatchVal) ? newValue : oldValue, self);
+      dirty = true;
     }
-  }, this);
+  });
+  return dirty;
 };

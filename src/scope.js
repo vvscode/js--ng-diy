135adb2af324ad78a022420c1fce1a5d18c5da0d
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-24 01:21:44 +0300

    ends the digest when the last watch is clean

--- src/scope.js
+++ src/scope.js
@@ -6,6 +6,7 @@ function initWatchVal() {
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
@@ -21,6 +22,7 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
 Scope.prototype.$digest = function() {
   var ttl = 10;
   var dirty;
+  this.$$lastDirtyWatch = null;
   do {
     dirty = this.$$digestOnce();
     if(dirty && !(ttl--)){
@@ -36,9 +38,12 @@ Scope.prototype.$$digestOnce = function() {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
+      self.$$lastDirtyWatch = watcher;
       watcher.last = newValue;
       watcher.listenerFn(newValue, (oldValue === initWatchVal) ? newValue : oldValue, self);
       dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;
     }
   });
   return dirty;

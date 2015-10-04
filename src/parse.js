0162c0ba5a8321c1b05dbbd21d5f07c6dccd6687
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 22:34:56 +0300

    Parse integers

---
+++ src/parse.js
@@ -0,0 +1,71 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function parse(expr) {
+  var lexer = new Lexer();
+  var parser = new Parser(lexer);
+  return parser.parse(expr);
+}
+
+function Lexer() {
+
+}
+
+Lexer.prototype.lex = function(text) {
+  this.text = text;
+  this.index = 0;
+  this.ch = undefined;
+  this.tokens = [];
+  while(this.index < this.text.length) {
+    this.ch = this.text.charAt(this.index);
+    if(this.isNumber(this.ch)){
+      this.readNumber();
+    } else {
+      throw 'Unexpected next character: ' + this.ch;
+    }
+  }
+
+  return this.tokens;
+};
+
+Lexer.prototype.isNumber = function(ch) {
+  return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function() {
+  var number = '';
+  while(this.index < this.text.length){
+    var ch = this.text.charAt(this.index);
+    if(this.isNumber(ch)){
+      number += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+  number = 1*number;
+  this.tokens.push({
+    text: number,
+    fn: _.constant(number),
+    constant: true
+  });
+};
+
+function Parser(lexer) {
+  this.lexer = lexer;
+}
+
+Parser.prototype.parse = function(text) {
+  this.tokens = this.lexer.lex(text);
+  return this.primary();
+};
+
+Parser.prototype.primary = function() {
+  var token = this.tokens[0];
+  var primary = token.fn;
+  if(token.constant) {
+    primary.constant = true;
+    primary.literal = true;
+  }
+  return primary;
+};
---
+++ test/parse_spec.js
@@ -0,0 +1,19 @@
+/* jshint globalstrict: true */
+/* global parse: false */
+'use strict';
+
+describy('parse', function() {
+
+  it('can parse an integer', function() {
+    var fn = parse('42');
+    expect(fn).toBeDefined();
+    expect(fn()).toBe(42);
+  });
+
+  it('makes integers both constant and literal', function() {
+    var fn = parse('42');
+    expect(fn.constant).toBe(true);
+    expect(fn.literal).toBe(true);
+  });
+
+});

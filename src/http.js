55645d12740ad631d504cea0556be567a829edbc
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-23 21:12:55 +0300

    Send requests via

--- src/http.js
+++ src/http.js
@@ -1,6 +1,38 @@
 /* jshint globalstrict: true */
 'use strict';
+
+function isSuccess(status) {
+  return status >= 200 && status < 300;
+}
+
 function $HttpProvider() {
-  this.$get = ['$httpBackend', function () {
+
+  this.$get = ['$httpBackend', '$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
+
+    return function (requestConfig) {
+      var deferred = $q.defer();
+
+      var config = _.extend({
+        method: 'GET'
+      }, requestConfig);
+
+      function done(status, response, statusText) {
+        status = Math.max(status, 0);
+        deferred[isSuccess(status) ? 'resolve' : 'reject']({
+          status: status,
+          data: response,
+          statusText: statusText,
+          config: config
+        });
+        if(!$rootScope.$$phase) {
+          $rootScope.$apply();
+        }
+      }
+
+      $httpBackend(config.method, config.url, config.data, done);
+      return deferred.promise;
+    };
+
   }];
+
 }
--- src/http_backend.js
+++ src/http_backend.js
@@ -1,6 +1,23 @@
 /* jshint globalstrict: true */
 'use strict';
+
 function $HttpBackendProvider() {
+
   this.$get = function () {
+    return function (method, url, post, callback) {
+      var xhr = new window.XMLHttpRequest();
+      xhr.open(method, url, true);
+      xhr.send(post || null);
+      xhr.onload = function () {
+        var response = ('response' in xhr) ? xhr.response :
+          xhr.responseText;
+        var statusText = xhr.statusText || '';
+        callback(xhr.status, response, statusText);
+      };
+      xhr.onerror = function () {
+        callback(-1, null, '');
+      };
+    };
   };
+
 }

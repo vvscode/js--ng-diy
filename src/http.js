12688c052ee9e4a0cab69da4473a048ea2bf7249
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-24 10:38:11 +0300

    allow  work with headers

--- src/http.js
+++ src/http.js
@@ -6,22 +6,105 @@ function isSuccess(status) {
 }
 
 function $HttpProvider() {
+  function executeHeaderFns(headers, config) {
+    return _.transform(headers, function (result, v, k) {
+      if(_.isFunction(v)) {
+        v = v(config);
+        if(_.isNull(v) || _.isUndefined(v)) {
+          delete result[k];
+        } else {
+          result[k] = v;
+        }
+      }
+    }, headers);
+  }
+
+  function mergeHeaders(config) {
+    var reqHeaders = _.extend(
+      {},
+      config.headers
+    );
+    var defHeaders = _.extend({},
+      defaults.headers.common,
+      defaults.headers[(config.method || 'get').toLowerCase()]
+    );
+    _.forEach(defHeaders, function (value, key) {
+      var headerExists = _.any(reqHeaders, function (v, k) {
+        return k.toLowerCase() === key.toLowerCase();
+      });
+      if(!headerExists) {
+        reqHeaders[key] = value;
+      }
+    });
+    return executeHeaderFns(reqHeaders, config);
+  }
+
+  function headersGetter(headers) {
+    var headersObj;
+    return function (name) {
+      headersObj = headersObj || parseHeaders(headers);
+      if(name) {
+        return headersObj[name.toLowerCase()];
+      } else {
+        return headersObj;
+      }
+    };
+  }
+
+  function parseHeaders(headers) {
+    var lines = headers.split('\n');
+    return _.transform(lines, function (result, line) {
+      var separatorAt = line.indexOf(':');
+      var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
+      var value = _.trim(line.substr(separatorAt + 1));
+      if(name) {
+        result[name] = value;
+      }
+    }, {});
+  }
+
+  var defaults = this.defaults = {
+    headers: {
+      common: {
+        Accept: 'application/json, text/plain, */*'
+      },
+      post: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      put: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      patch: {
+        'Content-Type': 'application/json;charset=utf-8'
+      }
+    }
+  };
 
   this.$get = ['$httpBackend', '$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
 
-    return function (requestConfig) {
+    function $http(requestConfig) {
       var deferred = $q.defer();
 
       var config = _.extend({
         method: 'GET'
       }, requestConfig);
+      config.headers = mergeHeaders(requestConfig);
 
-      function done(status, response, statusText) {
+      if(_.isUndefined(config.data)) {
+        _.forEach(config.headers, function (v, k) {
+          if(k.toLowerCase() === 'content-type') {
+            delete config.headers[k];
+          }
+        });
+      }
+
+      function done(status, response, headersString, statusText) {
         status = Math.max(status, 0);
         deferred[isSuccess(status) ? 'resolve' : 'reject']({
           status: status,
           data: response,
           statusText: statusText,
+          headers: headersGetter(headersString),
           config: config
         });
         if(!$rootScope.$$phase) {
@@ -29,10 +112,18 @@ function $HttpProvider() {
         }
       }
 
-      $httpBackend(config.method, config.url, config.data, done);
+      $httpBackend(
+        config.method,
+        config.url,
+        config.data,
+        done,
+        config.headers
+      );
       return deferred.promise;
-    };
+    }
 
+    $http.defaults = defaults;
+    return $http;
   }];
 
 }
--- src/http_backend.js
+++ src/http_backend.js
@@ -4,15 +4,22 @@
 function $HttpBackendProvider() {
 
   this.$get = function () {
-    return function (method, url, post, callback) {
+    return function (method, url, post, callback, headers) {
       var xhr = new window.XMLHttpRequest();
       xhr.open(method, url, true);
+      _.forEach(headers, function (value, key) {
+        xhr.setRequestHeader(key, value);
+      });
       xhr.send(post || null);
       xhr.onload = function () {
         var response = ('response' in xhr) ? xhr.response :
           xhr.responseText;
         var statusText = xhr.statusText || '';
-        callback(xhr.status, response, statusText);
+        callback(
+          xhr.status,
+          response,
+          xhr.getAllResponseHeaders(),
+          statusText);
       };
       xhr.onerror = function () {
         callback(-1, null, '');

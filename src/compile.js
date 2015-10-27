c7b238b1fe63b20e0f71ec4bef44b4686fca0c4f
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-27 20:34:55 +0300

    Compile nested directives ( recursivly for children )

--- src/compile.js
+++ src/compile.js
@@ -34,6 +34,9 @@ function $CompileProvider($provide) {
       _.forEach($compileNodes, function(node) {
         var directives = collectDirectives(node);
         applyDirectivesToNode(directives, node);
+        if (node.childNodes && node.childNodes.length) {
+          compileNodes(node.childNodes);
+        }
       });
     }
 

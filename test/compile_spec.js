61e37031f19580d917896744af2e8c50eae59a5f
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-31 14:57:36 +0300

    Add boolean attributes

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -554,5 +554,26 @@ describe('$compile', function() {
         expect(attrs1).toBe(attrs2);
       });
     });
+
+    it('sets prop for boolean attributes', function() {
+      registerAndCompile(
+        'myDirective',
+        '<input my-directive>', function(element, attrs) {
+          attrs.$set('disabled', true);
+          expect(element.prop('disabled')).toBe(true);
+        }
+      );
+    });
+
+    it('sets prop for boolean attributes even when not flushing', function() {
+      registerAndCompile(
+        'myDirective',
+        '<input my-directive>', function(element, attrs) {
+          attrs.$set('disabled', true, false);
+          expect(element.prop('disabled')).toBe(true);
+        }
+      );
+    });
+
   });
 });

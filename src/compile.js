9cc98ee4d9ab44e3ff87e2f91460dd2081073c66
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-03 19:43:01 +0300

    Link scope to compiled element

--- src/compile.js
+++ src/compile.js
@@ -142,7 +142,10 @@ function $CompileProvider($provide) {
     };
 
     function compile($compileNodes) {
-      return compileNodes($compileNodes);
+      compileNodes($compileNodes);
+      return function publicLinkFn(scope) {
+        $compileNodes.data('$scope', scope);
+      };
     }
 
     function compileNodes($compileNodes) {

cdac234da44b0c505c4b366d72f11792faaed0de
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-20 20:40:43 +0300

    transclusion with template urls

--- src/compile.js
+++ src/compile.js
@@ -95,7 +95,7 @@ function $CompileProvider($provide) {
     function compileTemplateUrl(directives, $compileNode, attrs, previousCompileContext) {
       var origAsyncDirective = directives.shift();
       var derivedSyncDirective = _.extend(
-        {}, origAsyncDirective, { templateUrl: null }
+        {}, origAsyncDirective, { templateUrl: null, transclude: null }
       );
       var templateUrl = _.isFunction(origAsyncDirective.templateUrl) ?
         origAsyncDirective.templateUrl($compileNode, attrs) :
@@ -110,16 +110,22 @@ function $CompileProvider($provide) {
         afterTemplateChildLinkFn = compileNodes($compileNode[0].childNodes);
         _.forEach(linkQueue, function(linkCall) {
           afterTemplateNodeLinkFn(
-            afterTemplateChildLinkFn, linkCall.scope, linkCall.linkNode);
+            afterTemplateChildLinkFn,
+            linkCall.scope,
+            linkCall.linkNode,
+            linkCall.boundTranscludeFn
+          );
         });
         linkQueue = null;
       });
 
-      return function delayedNodeLinkFn(_ignoreChildLinkFn, scope, linkNode) {
+
+      return function delayedNodeLinkFn(_ignoreChildLinkFn, scope, linkNode, boundTranscludeFn) {
         if (linkQueue) {
-          linkQueue.push({ scope: scope, linkNode: linkNode });
+          linkQueue.push({scope: scope, linkNode: linkNode, boundTranscludeFn: boundTranscludeFn});
         } else {
-          afterTemplateNodeLinkFn(afterTemplateChildLinkFn, scope, linkNode);
+          afterTemplateNodeLinkFn(
+            afterTemplateChildLinkFn, scope, linkNode, boundTranscludeFn);
         }
       };
     }
@@ -381,7 +387,8 @@ function $CompileProvider($provide) {
       var newIsolateScopeDirective = previousCompileContext.newIsolateScopeDirective;
       var controllerDirectives = previousCompileContext.controllerDirectives;
       var templateDirective = previousCompileContext.templateDirective;
-      var childTranscludeFn, hasTranscludeDirective;
+      var childTranscludeFn;
+      var hasTranscludeDirective = previousCompileContext.hasTranscludeDirective;
 
       function getControllers(require, $element) {
         if (_.isArray(require)) {
@@ -485,6 +492,7 @@ function $CompileProvider($provide) {
               templateDirective: templateDirective,
               newIsolateScopeDirective: newIsolateScopeDirective,
               controllerDirectives: controllerDirectives,
+              hasTranscludeDirective: hasTranscludeDirective,
               preLinkFns: preLinkFns,
               postLinkFns: postLinkFns
             }

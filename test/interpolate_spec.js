550c522e98ec362f5c106b0d1903332d4e27079e
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-22 16:43:09 +0300

    create interpolation function only if has expression

--- test/interpolate_spec.js
+++ test/interpolate_spec.js
@@ -88,4 +88,18 @@ describe('$interpolate', function() {
     expect(interp({ expr: 'value' })).toEqual('{{expr}} value {{expr}}');
   });
 
+  it('does not return function when flagged and no expressions', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+    var interp = $interpolate('static content only', true);
+    expect(interp).toBeFalsy();
+  });
+
+  it('returns function when flagged and has expressions', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+    var interp = $interpolate('has an {{expr}}', true);
+    expect(interp).not.toBeFalsy();
+  });
+
 });
